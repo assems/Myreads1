a8ffa26958c56ab39aac4f0e649a6c6ee9d65bd2
assems <36802233+assems@users.noreply.github.com>
2018-07-10 00:55:31 +0200

    Update Books.js

--- src/Books.js
+++ src/Books.js
@@ -24,11 +24,11 @@ class Books extends Component {
 
                                     <div className="book-shelf-changer">
                                         <select value={Book.shelf} onChange={(event) => updateBook(Book, event)}>
-                                            <option value="none" disabled>Move to...</option>
+                                            <option value="move" disabled>Move to...</option>
                                             <option value="currentlyReading">Currently Reading</option>
                                             <option value="wantToRead">Want to Read</option>
                                             <option value="read">Read</option>
-                                            <option value="none">None</option>
+                                            <option value="none" selected>None</option>
                                         </select>
                                     </div>
                             </div>
